de8f84269bf14dbc60a4553a0c61eea584cb2045
AAWorks <axalonso12@gmail.com>
2021-12-10 16:58:34 -0500

    main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,4 @@
 from flask import Flask, render_template, redirect
-from monclass import SUITS, VALS, Pokemon, ApiURLopener
+from gamerules import Actions, Round
 
-POKES = []
-
-def setup_pokes():
-    for i in range(1,53):
-        suit = SUITS[i%4]
-        val = VALS[i%13]
-        pokemon=Pokemon(str(i), suit, val)
-        POKES.append(pokemon)
-
+gamerules = Actions()
