7826d5aeb504d29d937a336f52ea639123b476e7
AAWorks <axalonso12@gmail.com>
2021-12-10 16:59:33 -0500

    updates to new py file

---
+++ app/gamerules.py
@@ -0,0 +1,49 @@
+from monclass import SUITS, VALS, Pokemon, ApiURLopener
+from urllib import request
+import json
+
+class Actions:
+    def __init__(self, user):
+        self.opener = ApiURLopener()
+
+        with self.opener.open("http://deckofcardsapi.com/api/deck/new/jokers_enabled=false") as response:
+            html = response.read()
+        self.deck = json.loads(html)["deck_id"]
+        self.deckAction("shuffle/")
+
+        self.POKES=[]
+        self.username=user
+        self.rounds=0
+
+        for i in range(1,53):
+            suit = SUITS[i%4]
+            val = VALS[i%13]
+            pokemon=Pokemon(str(i), suit, val)
+            self.POKES.append(pokemon)
+    
+    def deckAction(self, action):
+        with self.opener.open(f"http://deckofcardsapi.com/api/deck/{self.deck}/{action}") as response:
+            html = response.read()
+        return json.loads(html)
+
+    def drawCards(self):
+        def card_to_pokemon(self, code):
+            for poke in self.POKES:
+                if poke.deckname == code:
+                    return poke
+
+        drawn_pokemon = []
+        cards = self.deckAction("draw/?count=5")["cards"]
+        for card in cards:
+            drawn_pokemon.append(card_to_pokemon(card["code"]))
+        
+        return drawn_pokemon
+
+    def nextRound(self):
+        if self.current_round == 10:
+            self.gameover()
+        else:
+            self.current_round += 1
+    
+    def gameover(self):
+
