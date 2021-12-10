425197d34c3adc10f7e8cf6cc9ea8ec43e344120
AAWorks <axalonso12@gmail.com>
2021-12-10 08:10:38 -0500

    progress on pokemon class

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 from flask import flask, render_template, redirect
-from monclass import Pokemon
+import monclass
+
+POKES = []
+
+def setup_pokes():
+    names = []
+    for i in range(52):
+        suit = monclass.SUITS[i%4]
+        val = monclass.VALS[i%13]
+        pokemon=monclass.Pokemon(names[i], suit, val)
+        POKES.append(pokemon)
 
--- app/monclass.py
+++ app/monclass.py
@@ -1,6 +1,20 @@
 from urllib import request
 import json
 
+SUITS = ["hearts", "spades", "clubs", "diamonds"]
+VALS = ["a"] + list(range(2,11)) + ["J", "Q", "K"]
+
 class Pokemon:
-    def __init__(self):
-        
+    def __init__(self, name, suit, val):
+        types=[]
+
+        self.type1 = types[0]
+        self.type2 = types[1] if types[1] else None
+        self.name = name
+        self.deckname = str(val) + "-" + suit
+
+    def get_deckname(self):
+        return self.deckname
+
+    def __repr__(self):
+        return self.name
