c5c7ef99df613a0b330f6075faa5ea5167bf6a7a
AAWorks <axalonso12@gmail.com>
2021-12-10 08:34:36 -0500

    updates

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
-from flask import flask, render_template, redirect
+from flask import Flask, render_template, redirect
 import monclass
 
 POKES = []
 
 def setup_pokes():
-    
+
     ###put names code here pulling from api
 
-    for i in range(52):
+    for i in range(1):
         suit = monclass.SUITS[i%4]
         val = monclass.VALS[i%13]
         pokemon=monclass.Pokemon(names[i], suit, val)
