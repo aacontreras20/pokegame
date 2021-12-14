783673162d51a360c4b71b5a5b3b69a3fe4ffc09
AAWorks <axalonso12@gmail.com>
2021-12-13 23:13:38 -0500

    space

--- app/main.py
+++ app/main.py
@@ -8,12 +8,15 @@ app = Flask(__name__)
 @app.route('/home')
 def home():
     return render_template('home.html', title = "home")
+    
 @app.route('/about')
 def about():
     return render_template('about.html', title = "about")
+
 @app.route('/pokedex')
 def pokedex():
     return render_template('pokedex.html', title = "pokedex")
+    
 @app.route('/battle')
 def battle():
     return render_template('battle.html', title = "battle")
